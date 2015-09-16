e6c99a9103930f06b4b1cac769738fdd366ef4d2
Tim <zhenrong@buffalo.edu>
2015-09-16 18:10:43 -0400

    Added temporary login page, login button function

--- FairWellAndroid/app/src/main/java/io/github/budgetninja/fairwellandroid/MainActivity.java
+++ FairWellAndroid/app/src/main/java/io/github/budgetninja/fairwellandroid/MainActivity.java
@@ -4,13 +4,70 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.parse.LogInCallback;
+import com.parse.ParseException;
+import com.parse.ParseUser;
+
+import org.w3c.dom.Text;
+
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText username;
+    EditText password;
+    Button login_button;
+    Button register_button;
+    TextView forget_password;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        username = (EditText) findViewById(R.id.login_username);
+        password = (EditText) findViewById(R.id.login_password);
+        login_button = (Button) findViewById(R.id.login_button);
+        register_button = (Button) findViewById(R.id.register_button);
+        forget_password = (TextView) findViewById(R.id.forget_password);
+
+        login_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ParseUser.logInInBackground(username.getText().toString(), password.getText().toString(), new LogInCallback() {
+                    @Override
+                    public void done(ParseUser parseUser, ParseException e) {
+                        if(parseUser != null){
+                            //success, next activity
+                        }
+                        else{
+                            Toast.makeText(getApplicationContext(), "Incorrect Username or Password",Toast.LENGTH_LONG).show();
+                            password.setText("");
+                        }
+                    }
+                });
+            }
+        });
+
+        register_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        forget_password.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
     }
 
     @Override
